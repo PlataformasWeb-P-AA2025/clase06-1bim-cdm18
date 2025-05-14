f3207230dac32ea61d4018eb811320e83a8356e0
Carlos <cdmejia4@utpl.edu.ec>
2025-05-14 08:48:43 -0500

    final saludo2

--- ejemplo1/app_streamlit2.py
+++ ejemplo1/app_streamlit2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from sqlalchemy.orm import sessionmaker
-from crear_base import Saludo
+from crear_base import Saludo2
 from configuracion import engine
 
 # Crear sesión
@@ -8,21 +8,21 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Consultar docentes
-saludos = session.query(Saludo).all() # dentro ce parentesis clase involucrada de la cosulta
+saludos = session.query(Saludo2).all() # dentro ce parentesis clase involucrada de la cosulta
 
 # Mostrar con Streamlit
-st.title("Presentación de todos los Saludos")
+st.title("Presentación de todos los Saludos 2")
 
 for saludo  in saludos:
     st.write(saludo)
     st.markdown("---")
 
 st.markdown("---")
-st.title("Presentación de todos los Saludos en Tabla")
+st.title("Presentación de todos los Saludos 2 en Tabla")
 lista = []
 
 for s in saludos:
-    diccionario = {"id": s.id, "mensaje": s.mensaje, "tipo": s.tipo}
+    diccionario = {"id": s.id, "mensaje": s.mensaje, "tipo": s.tipo, "origen": s.origen}
     lista.append(diccionario)
 
 st.dataframe(lista)
